f54e3813289b501d6ea90e1dfe08ad2cfb080306
functionalStoic <7215306+functionalStoic@users.noreply.github.com>
2026-08-15 10:36:54 +0200

    feat(content): correct link to notes site

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
-  { href: 'https://publish.obsidian.md/functional-stoic', title: 'My Notes' },
+  { href: 'https://notes.jasonsooter.com', title: 'My Notes' },
   { href: '/blog', title: 'Blog' },
   { href: '/blog/uses', title: 'Uses' },
   { href: '/blog/bookshelf', title: 'Bookshelf' },
